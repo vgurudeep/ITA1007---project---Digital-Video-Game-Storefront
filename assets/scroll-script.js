3b6f6dfb1e54234935ded833842c199ce5f9cd9d
Gurudeep <54810880+vgurudeep@users.noreply.github.com>
2021-12-11 00:21:18 +0530

    Delete assets directory

--- assets/scroll-script.js
+++
@@ -1,6 +0,0 @@
-const arrow = document.querySelector('.down-arrow')
-const title = document.querySelector('.feature_main_title')
-
-arrow.addEventListener('click',()=> {
-    title.scrollIntoView({ behavior: 'smooth'});
-})
